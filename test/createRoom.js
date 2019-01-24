da23467504a7cc6e9054a58c50c0798c591689bf
Alberaan <alberaan@gmail.com>
2019-01-24 14:29:57 +0100

    First commit

---
+++ test/createRoom.js
@@ -0,0 +1,14 @@
+var assert = require("chai").assert;
+var room = require("../app/room");
+
+describe("Room tests for createRoom(size): ", function(){
+    it("Testing createRoom(1,1) is an array: ", function(){
+        myRoom = room.createRoom(1,1);
+        assert.typeOf(myRoom, 'array');
+    });
+    it("Testing createRoom(10,7) is 10x7: ", function(){
+        myRoom = room.createRoom(10,7);
+        assert.lengthOf(myRoom, 10);
+        assert.lengthOf(myRoom[0], 7);
+    });
+});
